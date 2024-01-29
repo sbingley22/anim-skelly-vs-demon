2159f930e9ef372c324dc5d5eeb2da8c6db87186
Sean B <seanb4@hotmail.co.uk>
2024-01-29 17:24:45 +0000

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { Suspense, useState } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
@@ -8,25 +9,45 @@ import * as Demon from './Demon'
 import * as Rock from './Rock'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [anim, setAnim] = useState(1)
+
+  const animClicked = (e) => {
+    if (e.button === 1) {
+      if (anim < 4) setAnim(prev => prev +1)
+      else setAnim(1)
+    }
+  }
 
   return (
     <>
       <Canvas
         shadows
+        onMouseDown={animClicked}
+        camera={{ position: [0,1,3]}}
       >
         <OrbitControls />
         <Suspense>
-          <Environment 
-            files="/UNDERWATER-HDRI.hdr"
+          {/* <Environment 
+            files="/background.hdr"
             background={false}
+            castShadow
+            backgroundIntensity={0.1}
+          /> */}
+          <ambientLight intensity={1.99} />
+          <spotLight 
+            position={[0, 10, 0]} 
+            angle={0.3} 
+            penumbra={1} 
+            castShadow 
+            intensity={500}
+            color="#EE9988"
           />
-          <ambientLight intensity={0.1} />
-          <spotLight position={[5, 12, -5]} angle={0.3} penumbra={1} castShadow />
 
-          <Skel.Model />
-          <Demon.Model />
-          <Rock.Model />
+          <group position={[0,-1,0]}>
+            <Skel.Model anim={anim} />
+            <Demon.Model anim={anim} />
+            <Rock.Model />
+          </group>
         </Suspense>
       </Canvas>
     </>
